b0dbfe6aa22096390e94d83990da4e257ac2fe84
the_atasoy <barisatasoy2917@outlook.com>
2023-12-09 16:52:46 +0300

    All crud operations has been done in EmployeeController class basically.

--- SpringDemo/Demo/demo/src/main/java/com/baris/demo/Controller/EmployeeController.java
+++ SpringDemo/Demo/demo/src/main/java/com/baris/demo/Controller/EmployeeController.java
@@ -1,5 +1,6 @@
 package com.baris.demo.Controller;
 
+import com.baris.demo.Model.Employee;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -11,13 +12,25 @@ public class EmployeeController {
 
     // getting data from client to server with path variable
     @GetMapping("/getEmployee/{id}")
-    public String getEmployee(@PathVariable("id") Long id) {
+    public String getEmployee(@PathVariable Long id) {
         return "You see the employee with id " + id + ".";
     }
 
     // getting data from client to server with request param
+    // this is delete request also, by this way anybody cannot access data using only browser
     @DeleteMapping("/deleteEmployee")
-    public String deleteEmployee(@RequestParam("id") Long id) {
+    public String deleteEmployee(@RequestParam Long id) {
         return "The employee with id " + id + " has been deleted.";
+    }
+
+    @PostMapping("/saveEmployee")
+    public String saveEmployee(@RequestBody Employee employee){
+        return employee.toString() + " has been saved.";
+    }
+
+    @PutMapping("/updateEmployee/{id}")
+    public Employee updateEmployee(@PathVariable Long id, @RequestBody Employee employee){
+        System.out.println("The employee with id " + id + " has been updated to " + employee.toString());
+        return employee;
     }
 }
---
+++ SpringDemo/Demo/demo/src/main/java/com/baris/demo/Model/Employee.java
@@ -0,0 +1,16 @@
+package com.baris.demo.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Employee {
+    private String name;
+    private Long age;
+    private String location;
+    private String email;
+    private String department;
+}
